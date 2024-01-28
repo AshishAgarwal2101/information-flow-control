7bb556714aa2494c5b2e90d3cfbaa390dba131b1
Ashish <ashish21.01agarwal@gmail.com>
2024-01-28 03:43:15 -0800

    Sem 3 end

--- packages/info-flow/index.js
+++ packages/info-flow/index.js
@@ -5,9 +5,12 @@ module.exports = {
     'my-custom-rule': {
       create(context) {
         // rule implementation
+        const variables = new Set();
+        const variableSecurityLevelMap = new Map();
+
         return {
           IfStatement(node) {
-              const services = utils.ESLintUtils.getParserServices(context);
+            const services = utils.ESLintUtils.getParserServices(context);
               const type = services.getTypeAtLocation(node.test.object);
               const scope = context.getScope();
               const symbol = (node.test.object?.name) ? services.getSymbolAtLocation(node.test.object) : null;
@@ -20,26 +23,87 @@ module.exports = {
                   if (isAssigningToPublic(scope, assignmentExpression)) {
                     context.report(assignmentExpression, 'Assignment to public variable detected in a protected block');
                   }
+                  // if(isSecurityUpgrade(scope, assignmentExpression)) {
+                  //   context.report(assignmentExpression, 'Security Upgrade detected');
+                  // }
                 });
               }
           },
-          FunctionDeclaration: function (node) {
-            if (node.id.name === 'leak') {
-              const params = node.params;
+          // FunctionDeclaration: function (node) {
+          //   if (node.id.name === 'leak') {
+          //     const params = node.params;
 
-              if (params.length !== 1 || params[0].name !== 'secret') {
+          //     if (params.length !== 1 || params[0].name !== 'secret') {
+          //       context.report({
+          //         node: node,
+          //         message: 'leak function signature must be leak(secret)'
+          //       });
+          //     }
+          //   }
+          // },
+          VariableDeclarator: function (node) {
+            const scope = context.getScope();
+            let isHighSecurity = node.init.callee?.name === "SecurityWrapper" && node.init.arguments[1].value === "H";
+            variableSecurityLevelMap.set(node.id.name, isHighSecurity ? "H" : "L");
+
+            //variables.add({scope, node});
+            //console.log("jkhjhg:: ", node);
+          },
+          AssignmentExpression: function (node) {
+            let scope = context.getScope();
+            //console.log("node:  ", node);
+            let isHighSecurity = node.right.callee?.name === "SecurityWrapper" && node.right.arguments[1].value === "H";
+            let oldSecurity = variableSecurityLevelMap.get(node.left.name);
+            if(oldSecurity !== "H" && isHighSecurity) {
+              context.report(node, 'Security Upgrade detected');
+            }
+            //isSecurityUpgrade(scope, node.left.name);
+          },
+          ThrowStatement(node) {
+            const services = utils.ESLintUtils.getParserServices(context);
+            const scope = context.getScope();
+            if (node.argument && node.argument.type === 'NewExpression' && node.argument.callee.name === 'Error') {
+              const errorMessageNode = node.argument.arguments && node.argument.arguments.length > 0 ? node.argument.arguments[0] : null;
+              const types = getVariableTypesFromSymbol(services, errorMessageNode);
+              console.log("Types: ", types);
+              if(types.includes("SecurityTypeHigh")) {
                 context.report({
-                  node: node,
-                  message: 'leak function signature must be leak(secret)'
+                  node,
+                  message: "High security values cannot be passed as an Error parameter",
                 });
               }
             }
-          }
+          },
+          // 'Program:exit': function () {
+          //   variables.forEach((variableData) => {
+          //     let {scope, node} = variableData;
+          //     //console.log("Variable:: ", variable);
+          //     isSecurityUpgrade(scope, node.id.name);
+          //   });
+          // },
+          // Program: function(node) {
+          //   const assignmentExpressions = getAllAssignments(node.consequent.body);
+          //   assignmentExpressions.forEach(assignmentExpression => {
+          //     if(isSecurityUpgrade(scope, assignmentExpression)) {
+          //       context.report(assignmentExpression, 'Security Upgrade detected');
+          //     }
+          //   });
+          // }
         };
       }
     }
   }
 }
+
+const getVariableTypesFromSymbol = (services, variableNode) => {
+  if(!services || !variableNode) return [];
+  const varType = services.getTypeAtLocation(variableNode.object);
+  let varTypes = [varType];
+  if(!varType.symbol) varTypes = varType.types ? varType.types : [];
+  const symbols = varTypes.map((type) => type?.symbol);
+  const types = symbols.map((symbol) => symbol?.escapedName);
+  return types.filter((type) => type);
+};
 
 const getVariableTypes = (scope, varName) => {
   const types = [];
@@ -72,14 +136,25 @@ const getVariableTypes = (scope, varName) => {
 
   //when type annotation present for the variable and multiple types declared
   const multipleTypeObjects = variable?.identifiers[0].typeAnnotation?.typeAnnotation?.types;
+  //console.log("multiple types:: ", multipleTypeObjects);
   if(!multipleTypeObjects) return types;
   return multipleTypeObjects
     .map((typeObj) => typeObj?.typeName?.name)
     .filter((typeName) => typeName);
 };
 
+const getSecurityLevelOfAssignment = (assignmentExpression) => {
+  let variableDefinition = assignmentExpression.right;
+  if(variableDefinition.type === "CallExpression" && variableDefinition?.callee?.name === "SecurityWrapper") {
+    if(variableDefinition.arguments[1].value === "H") return "H";
+  }
+
+  return "L";
+};
+
 const getNodeObjects = (node) => {
-  if(node.object) return [node.object];
+  if(node.type === "Identifier") return [node];
+  if(node.object?.type === "Identifier") return [node.object];
   if(!node.left && !node.right) return [];
 
   let allNodeObjects = [];
@@ -92,7 +167,6 @@ const getNodeObjects = (node) => {
 
 function containsHighSecurityIdentifier(scope, node) {
   let nodeObjects = getNodeObjects(node);
-  
   for(let i=0; i<nodeObjects.length; i++) {
     let nodeObj = nodeObjects[i];
     
@@ -130,7 +204,7 @@ const getObjectNodesFromExpression = (node) => {
 
 const getAllAssignments = (nodes) => {
   const expressions = nodes.map((node) => node.expression);
-  return expressions.filter((expression) => expression.type === 'AssignmentExpression');
+  return expressions.filter((expression) => expression && expression.type === 'AssignmentExpression');
 };
 
 const isAssigningToPublic = (scope, assignmentExpression) => {
@@ -138,8 +212,35 @@ const isAssigningToPublic = (scope, assignmentExpression) => {
     let variableTypes = getVariableTypes(scope, assignmentExpression.left.name);
     // console.log("Variable types for " + assignmentExpression.left.name + " = ");
     // console.log(variableTypes, "\n");
-    return !variableTypes.includes("SecurityTypeHigh");
+    return !variableTypes.includes("SecurityTypeHigh") && getSecurityLevelOfAssignment(assignmentExpression) !== "H";
   };
 
   return false;
 }
+
+const isSecurityUpgrade = (scope, varName) => {
+  const variable = scope.variables.find(v => v.name === varName);
+  let variableDefinitions = (variable?.defs && variable.defs.length > 0) ? (variable.defs.map((variableDef) => variableDef?.node?.init)) : null
+  console.log("var defs for : " + varName, variableDefinitions);
+
+  let lastSecurityLevel = null;
+  for(let variableDef in variableDefinitions) {
+    let isHighSecurity = variableDef?.callee?.name === "SecurityWrapper" && variableDef.arguments[1].value === "H";
+    if(lastSecurityLevel === null) {
+      lastSecurityLevel = isHighSecurity ? "H" : "L";
+    }
+    else if(lastSecurityLevel === "L" && isHighSecurity) {
+      return true;
+    }
+  }
+
+  return false;
+};
+
+const isSecurityUpgrade1 = (scope, assignmentExpression) => {
+  //console.log("Assignment: ", assignmentExpression);
+  let oldLevel = getVariableTypes(scope, assignmentExpression.left.name).includes("SecurityTypeHigh") ? "H" : "L";
+  let newLevel = getSecurityLevelOfAssignment(assignmentExpression);
+  if(newLevel === "H" && oldLevel === "L") return true;
+  return false;
+};
--- src/informationLeakProblem.ts
+++ src/informationLeakProblem.ts
@@ -1,17 +1,17 @@
 import { SecurityWrapper, SecurityTypeHigh, SecurityTypeLow } from "./SecurityTypes";
 
-const StringHProxy1 = SecurityWrapper("Private String 1", "H");
-const StringLProxy2 = SecurityWrapper("Public String 2", "L");
+// const StringHProxy1 = SecurityWrapper("Private String 1", "H");
+// const StringLProxy2 = SecurityWrapper("Public String 2", "L");
 
-console.log(StringHProxy1);
-console.log(StringLProxy2);
-const ch = StringHProxy1.charAt(2);
-console.log("Extracted char = ", ch);
-const subSt = StringLProxy2.substring(0, 6);
-console.log("Extracted substring = ", subSt);
-const concatStr = StringHProxy1.concat(" ").concat(StringLProxy2);
-console.log("Concatenated String = ", concatStr);
-console.log(concatStr instanceof SecurityTypeHigh);
+// console.log(StringHProxy1);
+// console.log(StringLProxy2);
+// const ch = StringHProxy1.charAt(2);
+// console.log("Extracted char = ", ch);
+// const subSt = StringLProxy2.substring(0, 6);
+// console.log("Extracted substring = ", subSt);
+// const concatStr = StringHProxy1.concat(" ").concat(StringLProxy2);
+// console.log("Concatenated String = ", concatStr);
+// console.log(concatStr instanceof SecurityTypeHigh);
 //console.log(StringHProxy1 + StringHProxy2);
 
 
@@ -31,14 +31,19 @@ function leak1(secret: SecurityTypeHigh | SecurityTypeLow) {
     return y;
 }
 
+// function leakTest() {
+//     let x = SecurityWrapper(true, "L");
+//     x = true;
+//     x = SecurityWrapper
+// }
+
 function leak2(secret: SecurityTypeHigh | SecurityTypeLow) {
-    let x: any = true;
+    let x = true;
     if(secret.securityValue) {
-        x = SecurityWrapper(false, "H");
+        x = SecurityWrapper(true, "H");
     }
 
     let y = false;
-    //console.log("----", typeof x);
     if(x !== true) {
         y = true;
     }
@@ -51,3 +56,22 @@ function leak2(secret: SecurityTypeHigh | SecurityTypeLow) {
 
 console.log("--------------------------------INFORMATION LEAK PROBLEM------------------------------------------");
 console.log(leak2(SecurityWrapper(false, "H")));
+
+function leak3(secret: SecurityTypeHigh | SecurityTypeLow) {
+    if(secret.securityValue > 5) {
+        throw new Error(secret.securityValue);
+    }
+
+    //below, even if exception not thrown, information is leaked that secret's value is less than 5 (assuming sourve code is available with the hacker)
+    //TODO: prevent the below leak
+    return true;
+}
+
+let isGreaterThanFive = false;
+try {
+    leak3(SecurityWrapper(10, "H"));
+} catch(err) {
+    isGreaterThanFive = true;
+    let lowLeaked = err.message;
+    console.log(lowLeaked);
+}
